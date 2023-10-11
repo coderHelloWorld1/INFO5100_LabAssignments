89413a37f22d7f8140e89868e953a5fd65c03c1a
coderHelloWorld1 <shethpurvam333@gmail.com>
2023-10-11 19:06:10 -0400

    Lab-4 Created Patient Model

---
+++ LAB4/INFO5100_LabAssignment4/src/model/patientModel.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import javax.swing.Icon;
+/**
+ *
+ * @author Purvam Sheth
+ */
+public class patientModel {
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the typeOfPatient
+     */
+    public String getTypeOfPatient() {
+        return typeOfPatient;
+    }
+
+    /**
+     * @param typeOfPatient the typeOfPatient to set
+     */
+    public void setTypeOfPatient(String typeOfPatient) {
+        this.typeOfPatient = typeOfPatient;
+    }
+
+    /**
+     * @return the image
+     */
+    public Icon getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(Icon image) {
+        this.image = image;
+    }
+        private String firstName;
+        private String lastName;
+        private String age;
+        private String email;
+        private String message;
+        private String gender;
+        private String typeOfPatient;
+        private Icon image;    
+}
